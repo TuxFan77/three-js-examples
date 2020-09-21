1f1f9b4d77b7bc3e1fc7a0016c1f5e8ed1744924
Brian Cross <briancross@shaw.ca>
2020-09-21 16:56:26 -0600

    added light to scene

--- src/example02/index.js
+++ src/example02/index.js
@@ -18,12 +18,18 @@ camera.position.z = 2;
 
 const scene = new THREE.Scene();
 
+const color = 0xffffff;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
 const boxWidth = 1;
 const boxHeight = 1;
 const boxDepth = 1;
 const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
 
 const cube = new THREE.Mesh(geometry, material);
 
