d3b00a095f0cf0878651d6bcb251ff90184846d6
Brian Cross <briancross@shaw.ca>
2020-09-21 13:54:09 -0600

    initial commit

---
+++ src/example01/index.js
@@ -0,0 +1,32 @@
+import "../threejs.scss";
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, getWindowAspect(), 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+camera.position.z = 5;
+
+function getWindowAspect() {
+  return window.innerWidth / window.innerHeight;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
